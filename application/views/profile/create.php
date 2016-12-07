dbbd6a6ccec99a88172fa543827eb65e559471b2
Ainul Yakin <ainul086@gmail.com>
2016-12-07 15:00:21 +0700

    edit tampilan form

--- application/views/profile/create.php
+++ application/views/profile/create.php
@@ -1,18 +1,36 @@
-<form action="<?= site_url('profile/store') ?>" method="POST">
-    <div class="form-group">
-        <label for="name">Nama Profil</label>
-        <?php echo form_error('name'); ?>
-        <input type="text" class="form-control" id="name" name="name" value="<?= (isset($profile['name'])) ? $profile['name'] : ''; ?>">
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Tambah Profil</h3>
     </div>
-    <div class="form-group">
-        <label for="headline">Judul Profil</label>
-        <?php echo form_error('headline'); ?>
-        <input type="text" class="form-control" id="headline" name="headline" value="<?= (isset($profile['headline'])) ? $profile['headline'] : ''; ?>">
+    <div class="panel-body">
+        <form class="form-horizontal" action="<?= site_url('profile/store') ?>" method="POST">
+            <div class="form-group">
+                <label for="name" class="col-sm-2 control-label">Nama Profil</label>
+                <div class="col-sm-10">
+                    <?php echo form_error('name'); ?>
+                    <input type="text" class="form-control" id="name" name="name" value="<?= (isset($profile['name'])) ? $profile['name'] : ''; ?>" placeholder="nama profil" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="headline" class="col-sm-2 control-label">Judul Profil</label>
+                <div class="col-sm-10">
+                    <?php echo form_error('headline'); ?>
+                    <input type="text" class="form-control" id="headline" name="headline" value="<?= (isset($profile['headline'])) ? $profile['headline'] : ''; ?>" placeholder="judul profil" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="body" class="col-sm-2 control-label">Isi Profil</label>
+                <div class="col-sm-10">
+                    <?php echo form_error('body'); ?>
+                    <textarea class="form-control" id="body" name="body" placeholder="isi profil"><?= (isset($profile['headline'])) ? $profile['headline'] : ''; ?></textarea>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm10">
+                    <button class="btn btn-primary" type="submit">Simpan</button>
+                    <button class="btn btn-warning" type="reset">Batal</button>
+                </div>
+            </div>
+        </form>
     </div>
-    <div class="form-group">
-        <label for="body">Isi Profil</label>
-        <?php echo form_error('body'); ?>
-        <textarea class="form-control" id="body" name="body"><?= (isset($profile['headline'])) ? $profile['headline'] : ''; ?></textarea>
-    </div>
-    <button type="submit" class="btn btn-default">Submit</button>
-</form>
+</div>

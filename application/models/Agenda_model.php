920ea79a1e0134dd0ba17a2dba364627eddb08e6
Andre Hardika <andrehardika@gmail.com>
2016-12-08 16:17:54 +0700

    features/agenda/Add eloquent details at Agenda_model

--- application/models/Agenda_model.php
+++ application/models/Agenda_model.php
@@ -8,6 +8,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Agenda_model extends MY_Model
 {
+    public $primary_key = 'id';
+    public $fillable = array('name','body','agenda_date');
+    public $protected = array('id');
+
 	public $rules = array(
         'insert' => array(
             'name' => array(

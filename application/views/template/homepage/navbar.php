68fb773ce4b06c683a5e9c82f7f63603e645d551
Ainul Yakin <ainul086@gmail.com>
2016-12-26 10:46:35 +0700

    Buat tampilan news dan news_article di halaman homepage

--- application/views/template/homepage/navbar.php
+++ application/views/template/homepage/navbar.php
@@ -25,7 +25,7 @@
             <li><a href="#">Sekar Putih</a></li>
           </ul>
         </li>
-        <li><a href="#">Berita</a></li>
+        <li><a href="<?=site_url('homepage/news')?>">Berita</a></li>
         <li><a href="#">Agenda</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Galeri <span class="caret"></span></a>
